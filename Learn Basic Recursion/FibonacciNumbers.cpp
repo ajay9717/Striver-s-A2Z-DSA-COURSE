1ff462b2551ce7a83dc705efdaf5324aaf72942e
Ajay Kumar <ajaykumar123asdf@gmail.com>
2024-04-29 11:10:45 +0530

    updated

--- Learn Basic Recursion/FibonacciNumbers.cpp
+++ Learn Basic Recursion/FibonacciNumbers.cpp
@@ -1,17 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int fibonacci(int n)
+{
+    if(n <= 1)
+    return n;
+    else
+    return fibonacci(n-1) + fibonacci(n-2);
+}
 int main()
 {
-    int n;
-    cout << "Enter the number of fiboncacci series you want";
-    cin >> n;
+    int fib;
+    cout << "Enter the index of which fibonacci number you want";
+    cin >> fib;
+    cout << fibonacci(fib);
 
-    int f = 0;
-    int s = 1;
-
-    for(int i = 0; i < n; i++)
-    {
-        ans = f[i-1] + f[i-2];
-    }
 }
